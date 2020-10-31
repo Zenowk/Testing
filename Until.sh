c1ab1c4a409c4897e9b4cbf086193587136f5555
BBaldwin99 <71043825+BBaldwin99@users.noreply.github.com>
2020-10-31 16:27:50 -0500

    Update Until.sh

--- Until.sh
+++ Until.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#This until loop prints numbers decrementing by 1 until the value of the
+#loop control variable, counter, is equal to 2. It starts at 11 because
+#the loop decrements counter by 1 before printing the number.
+
+#I find this loop interesting because Java doesn't have this loop, and I
+#think that it performs a useful function which is the opposite of the
+#while loop, by continuing to execute the block of code as long as the
+#condition is false, not true.
+
 counter=12
 until [ $counter -lt 3 ]; do
 	let counter-=1
 	echo $counter
 done
-
