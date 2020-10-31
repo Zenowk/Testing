b8adf79569b9d09e9b482ed9dfa464724a67398e
BBaldwin99 <71043825+BBaldwin99@users.noreply.github.com>
2020-10-31 16:07:54 -0500

    Update While.sh

--- While.sh
+++ While.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#This while loop prints numbers incrementing by 1 until the value of the
+#loop control variable, counter, is equal to 4, starting at 1. So, this
+#will print 1, 2, 3, and 4 and the loop terminates.
+
+#I find this loop interesting in that in bash, -lt is the conditional to
+#indicate less than, and that even though the condition set is less than
+#4, it will still print 4, even though 4 is equal to and not less than the
+#condition set.
+
 counter=0
 while [ $counter -lt 4 ]; do
 	let counter+=1
 	echo $counter
 done
-
